fae262cc5ade4d41f08a84e912ed8af62d8fbfda
faisalinfinity <mujtabafaisal944.fk@gmail.com>
2023-03-26 20:26:33 +0530

    changes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { RegisterRoute } = require("./Routes/RegisterRoute");
 const connection = require("./Connection/connection");
 const { AlluserRoute } = require("./Routes/AlluserRoute");
 const { NotesRoute } = require("./Routes/NotesRoute");
+const { default: mongoose } = require("mongoose");
 const app = express();
 
 app.use(cors());
@@ -15,11 +16,26 @@ app.use("/login", LoginRoute);
 app.use("/users",AlluserRoute)
 app.use("/notes",NotesRoute)
 
-app.listen(process.env.PORT, async () => {
-  try {
-    await connection;
-    console.log("Connected to MONGO",`Running on PORT ${process.env.PORT}`);
-  } catch (error) {
-    console.log(error);
-  }
+
+mongoose.connect(process.env.ATLAS, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+  app.listen(process.env.PORT, () => {
+    console.log('Server started on port 8080');
+  });
+})
+.catch((error) => {
+  console.error('Error connecting to MongoDB', error);
 });
+
+// app.listen(, async () => {
+//   try {
+//     await connection;
+//     console.log("Connected to MONGO",`Running on PORT ${process.env.PORT}`);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
